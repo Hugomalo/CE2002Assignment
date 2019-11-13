44364ba898a9df7f8807a416514af0f4ed5a8062
Hugomalo <hbutery@gmail.com>
2019-11-13 12:44:28 +0100

    Updated calendar

--- user/Calendar.java
+++ user/Calendar.java
@@ -1,12 +1,12 @@
 package user;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Calendar {
-    private static LocalDateTime[] SunPH; //Stores sundays and public holidays in an Array
+    private static ArrayList<LocalDateTime> SunPH; //Stores sundays and public holidays in an Array
 
-    public LocalDateTime[] getSunPH(){
+    public ArrayList<LocalDateTime> getSunPH(){
         return SunPH;
     }
-    public void setSelectedDate(){}
 }
