4d7d1d76e3c5b3edc720deebc8619262076e18e2
chian95 <57388525+chian95@users.noreply.github.com>
2019-11-17 22:37:36 +0800

    Added Javadoc

--- MOBLIMA/admin/MovieListing.java
+++ MOBLIMA/admin/MovieListing.java
@@ -1,7 +1,6 @@
 package MOBLIMA.admin;
 
 import javax.lang.model.type.NullType;
-
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -9,20 +8,38 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ * MovieListing class for the list of movies.
+ * @author CE2002 SE3 Group 4
+ */
+
 public class MovieListing implements Serializable {
 	private static final String filepath = "listing" + File.separator + "movies.ser";
     private static final long serialVersionUID = 1L;
 
     private static ArrayList<Movie> movies = (ArrayList<Movie>) MOBLIMA.ObjectsIO.ReadObject(MovieListing.filepath);
 
+    /**
+     * Gets the filepath of the movie listing.
+     * @return Filepath
+     */
     public static String getFilepath(){
         return filepath;
     }
 
+    /**
+     * Gets a list of movies.
+     * @return List of movies
+     */
     public static ArrayList<Movie> getMovies(){
         return movies;
     }
 
+    /**
+     * Gets the movie based on the title.
+     * @param title Movie title
+     * @return Movie
+     */
     public static Movie getMovie(String title){
         for (Movie m : movies){
             if (m.getTitle().equals(title)){
@@ -32,11 +49,18 @@ public class MovieListing implements Serializable {
         return null;
     }
 
+    /**
+     * Displays a list of all movies.
+     */
     public static void showMovies(){
         for (Movie movie : movies){
             System.out.println(movie.getTitle());
         }
     }
+    
+    /**
+     * Displays a list of movies that are not under 'End Of Showing'
+     */
     public static void showMoviesShowing(){
         for (Movie movie : movies){
             if (movie.getShowingStatus() != Movie.ShowingStatus.End_Of_Showing) {
@@ -45,6 +69,10 @@ public class MovieListing implements Serializable {
         }
     }
 
+    /**
+     * Displays the top 5 ranking movies by either overall rating or ticket sales.
+     * @param r Boolean variable
+     */
     public static void showRanking(boolean r){
         if (r){
             ArrayList<Movie> rankedMovies = new ArrayList<Movie>(movies);
@@ -68,16 +96,30 @@ public class MovieListing implements Serializable {
         }
     }
 
+    /**
+     * Adds a movie to the list.
+     * @param m Movie
+     */
     protected static void addMovie(Movie m){
         if (m == null){
-            System.out.println("Null problème...");
+            System.out.println("No movies");
         }
         movies.add(m);
     }
+    
+    /**
+     * Removes a movie from the list.
+     * @param m Movie
+     */
     protected static void removeMovie(Movie m){
         movies.remove(m);
     }
 
+    /**
+     * Gets the movie details; if movie title is found, the movie information is displayed.
+     * @param t Movie title
+     * @return Boolean result; true if movie title is found
+     */
     public static boolean getMovieDetails(String t){
         for (Movie movie : movies) {
             if (movie.getTitle().equals(t)) {
--- MOBLIMA/admin/Seat.java
+++ MOBLIMA/admin/Seat.java
@@ -2,6 +2,11 @@ package MOBLIMA.admin;
 
 import java.io.Serializable;
 
+/**
+ * Seat class for seat information (in rows and columns).
+ * @author CE2002 SE3 Group 4
+ */
+
 public class Seat implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -22,12 +27,25 @@ public class Seat implements Serializable {
     private Row row;
     private int column;
 
+    /**
+     * Cinema seat with an assigned row and column.
+     * @param r Row
+     * @param c Column
+     */
     Seat(Row r,int c)
     {
         row=r;
         column=c;
     }
 
+    /**
+     * Gets the row of a seat.
+     * @return Row
+     */
     public Row getRow(){return row;}
+    /**
+     * Gets the column of a seat.
+     * @return Column
+     */
     public int getColumn(){return column;}
 }
