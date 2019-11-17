57e50353d9e8a0128f33ffe362c92554dd39f43f
LeafGecko <theleafgecko@gmail.com>
2019-11-17 17:18:22 +0800

    I/O route comments if using linux system

--- MOBLIMA/user/Booking.java
+++ MOBLIMA/user/Booking.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 public class Booking implements Serializable {
     private static final long serialVersionUID = 1L;
     private static final String filepath="listing\\bookings.ser";
+    // if you are using linux, change to "listing/bookings.ser"
 
     public static String getFilepath() {
         return filepath;
